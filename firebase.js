f378e3c5f7dd1ecadd1c713492f5e2c7cae07b90
juwoncaleb <calebomojuwon@gmail.com>
2021-12-12 13:24:22 +0100

    new changes

--- firebase.js
+++ firebase.js
@@ -1,4 +1,4 @@
-import firebase from "firebase";
+import firebase from "firebase/compat/app";
 const firebaseConfig = {
   apiKey: "AIzaSyCAZmwfjsibpfwpAnIENjhlMby55yt3kek",
   authDomain: "fashion-5b4e9.firebaseapp.com",
--- src/pages/auth/FirebaseAuth.js
+++
@@ -1,3 +0,0 @@
-import initFirebase from "../../firebase/initFirebase";
-import { useEffect , useState } from "react";
-import StyledFirebaseAuth from "react-firebaseui/styledFirebaseAuth";
