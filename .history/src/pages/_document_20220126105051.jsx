93b785d464e6c56420fcf795d64a7a1eeb3e8da1
juwoncaleb <calebomojuwon@gmail.com>
2022-01-26 11:04:58 +0100

    new

--- .history/src/pages/_document_20220126105051.jsx
+++ .history/src/pages/_document_20220126105051.jsx
@@ -17,7 +17,6 @@ import Document, {
       return (
         <Html>
           <Head>
-            
           <link href="https://fonts.googleapis.com/css2?family=Montserrat:ital,wght@0,100;0,200;0,300;0,400;0,500;1,100;1,200;1,300;1,400;1,600&display=swap" rel="stylesheet"/>
 
           </Head>
